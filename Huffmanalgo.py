9150d3def6ec5eeea982589c963ffba46ac3ea5f
HalalHacker <122071878+mohd-shaamii@users.noreply.github.com>
2023-07-11 15:30:46 +0530

    Update Huffmanalgo.py

--- Huffmanalgo.py
+++ Huffmanalgo.py
@@ -1,75 +1,94 @@
-# A Huffman Tree Node
 import heapq
 
-class node:
+class Node:
     def __init__(self, freq, symbol, left=None, right=None):
-        # frequency of symbol
         self.freq = freq
-        # symbol name (character)
         self.symbol = symbol
-        # node left of current node
         self.left = left
-        # node right of current node
         self.right = right
-        # tree direction (0/1)
         self.huff = ''
+
     def __lt__(self, nxt):
         return self.freq < nxt.freq
 
-# utility function to print huffman
-# codes for all symbols in the newly
-# created Huffman tree
 def printNodes(node, val=''):
-    # huffman code for current node
     newVal = val + str(node.huff)
-    # if node is not an edge node
-    # then traverse inside it
-    if node.left :
+    if node.left:
         printNodes(node.left, newVal)
-    if(node.right):
-        printNodes(node.right,newVal)
-        # if node is edge node then
-        # display its huffman code
-    if(not node.left and not node.right):
+    if node.right:
+        printNodes(node.right, newVal)
+    if not node.left and not node.right:
         print(f"{node.symbol} -> {newVal}")
+def encode_text(text, huffman_tree):
+    encoding_map = {}
 
-# characters for huffman tree
-chars = input("").split(' ') #['a', 'b', 'c', 'd', 'e', 'f']
+    def traverse_tree(node, code=''):
+        if node.left:
+            traverse_tree(node.left, code + '0')
+        if node.right:
+            traverse_tree(node.right, code + '1')
+        if not node.left and not node.right:
+            encoding_map[node.symbol] = code
 
-# frequency of characters
-freq = [int(i) for i in input("").split(' ')] #[5, 9, 12, 13, 16, 45]
+    traverse_tree(huffman_tree)
 
-# list containing unused nodes
+    encoded_text = ''
+    for char in text:
+        encoded_text += encoding_map[char]
+
+    return encoded_text
+
+def decode_text(encoded_text, huffman_tree):
+    decoded_text = ''
+    current_node = huffman_tree
+
+    for bit in encoded_text:
+        if bit == '0':
+            current_node = current_node.left
+        else:
+            current_node = current_node.right
+
+        if not current_node.left and not current_node.right:
+            decoded_text += current_node.symbol
+            current_node = huffman_tree
+
+    return decoded_text
+
+chars = input("Enter characters: ").split(' ')
+freq = [int(i) for i in input("Enter frequencies: ").split(' ')]
+
 nodes = []
 
-# converting characters and frequencies
-# into huffman tree nodes
 for x in range(len(chars)):
-    heapq.heappush(nodes, node(freq[x], chars[x]))
+    heapq.heappush(nodes, Node(freq[x], chars[x]))
 
 while len(nodes) > 1:
-    # sort all the nodes in ascending order
-    # based on their frequency
     left = heapq.heappop(nodes)
     right = heapq.heappop(nodes)
-    # assign directional value to these nodes
-    left.huff = 0
-    right.huff = 1
-    # combine the 2 smallest nodes to create
-    # new node as their parent
-    newNode = node(left.freq+right.freq, left.symbol+right.symbol, left, right)
-    heapq.heappush(nodes, newNode)
-
-# Huffman Tree is ready!
+    left.huff = '0'
+    right.huff = '1'
+    new_node = Node(left.freq + right.freq, left.symbol + right.symbol, left, right)
+    heapq.heappush(nodes, new_node)
 printNodes(nodes[0])
 
-"""output
-a b c d e f
-5 9 12 13 16 45
-f -> 0
-c -> 100
-d -> 101
-a -> 1100
-b -> 1101
-e -> 111
+huffman_tree = nodes[0]
+
+encoded_text = input("Enter the encoded text: ")
+decoded_text = decode_text(encoded_text, huffman_tree)
+print("Decoded text:", decoded_text)
+text_input = input("Enter text to encode: ")
+encoded_binary_text = encode_text(text_input, huffman_tree)
+print("Encoded binary text:", encoded_binary_text)
+
 """
+Enter characters: a b c d e
+Enter frequencies: 5 10 8 15 12
+b -> 00
+e -> 01
+a -> 100
+c -> 101
+d -> 11
+Enter the encoded text: 0010011
+Decoded text: bad
+Enter text to encode: dad
+Encoded binary text: 1110011 """
