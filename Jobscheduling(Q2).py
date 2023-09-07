165f7b9c7cb8fccb3fcc3fa12a48b6f18f65673f
BitRebels <122071878+mohd-shaamii@users.noreply.github.com>
2023-09-07 21:22:03 +0530

    Update Jobscheduling(Q2).py

--- Jobscheduling(Q2).py
+++ Jobscheduling(Q2).py
@@ -25,3 +25,27 @@ if __name__=='__main__':
         jobs.append(Job(taskId,deadline,profit))
     T=int(input("Enter the total time: "))
     scheduleJobs(jobs,T)
+
+
+# Enter the number :6
+# Enter the taskId of 1 : 1
+# Enter the profit of 1 : 200
+# Enter the deadline of 1 : 2
+# Enter the taskId of 2 : 2
+# Enter the profit of 2 : 180
+# Enter the deadline of 2 : 3
+# Enter the taskId of 3 : 3
+# Enter the profit of 3 : 120
+# Enter the deadline of 3 : 2
+# Enter the taskId of 4 : 4
+# Enter the profit of 4 : 100
+# Enter the deadline of 4 : 3
+# Enter the taskId of 5 : 5
+# Enter the profit of 5 : 50
+# Enter the deadline of 5 : 4
+# Enter the taskId of 6 : 6
+# Enter the profit of 6 : 30
+# Enter the deadline of 6 : 1
+# Enter the Timeline4
+# Scheduled jobs are : [3, 1, 2, 5]
+# The total Profit : 550
